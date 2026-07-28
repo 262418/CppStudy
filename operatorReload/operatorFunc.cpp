6b4e6ed7f14d6dc2f200950a00ed8bf78074e3ab
嘴角流下的泪水 <156180986+262418@users.noreply.github.com>
2026-07-28 22:31:21 +0800

    仿函数——重载"()"运算符

---
+++ operatorReload/operatorFunc.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+class Show
+{
+public:
+    void operator()(const string str)
+    {
+        cout<<str<<endl;
+    }
+    float operator()(const float num)
+    {
+        return num*num;
+    }
+};
+int main()
+{
+    //仿函数指的是在类中重载"()"运算符后，这个类的对象可以像函数一样使用
+    Show s;
+    s("ABCD");
+    cout<<s(3)<<endl;
+    return 0;
+}
